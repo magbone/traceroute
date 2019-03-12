5c8cb9e33e13f2e67d4b6bec9c85db2bc5fecbad
magbone <2300427529@qq.com>
2019-03-12 20:36:27 +0800

    modify test.c

--- test/test.c
+++ test/test.c
@@ -6,9 +6,14 @@ int error_callback(char *msg)
       return 1;
 }
 
-int success_callback(char *dest, long long *mss)
+int success_callback(char *dest, long long *mss, INFO info)
 {
+      if(info == OK)
       printf("%s\t\t%lldms\t%lldms\t%lldms\n", dest, mss[0], mss[1], mss[2]);
+      else if(info == TIME_OUT)
+      printf("*.*.*.*\t\t\t*\t*\t*\t\n");
+      else if(info == FINISHED)
+      printf("finished\n");
       return 1;
 }
 
@@ -24,7 +29,7 @@ int main(int argc,char *argv[])
       }
       t->cmd.protocol = UDP;
       t->cmd.ttl = 45;
-      t->cmd.addr = "43.254.218.121";
+      t->cmd.addr = "114.114.114.114";
       traceroute_run_async(t, success_callback, error_callback);
       return 0;
 }
